67112e5c52953f9d5291eb87d3fa35407250eed8
Danny Han <codeoftoad@gmail.com>
2024-06-11 08:33:08 -0400

    Add learning file

---
+++ sketch_jun10a/sketch_jun10a.ino
@@ -0,0 +1,42 @@
+int   redLED = 8;
+float pi    = 3.14;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+  pinMode(8, OUTPUT);
+
+};
+
+/**
+ * uint8_t pin   == pin number
+ * uint8_t count == how many times to blink
+ */
+void blinkQuickly(uint8_t pin, uint8_t count) {
+  for (int i = 0; i < count; i++) {
+    digitalWrite(pin, HIGH);
+    delay(75);
+    digitalWrite(pin, LOW);
+    delay(350);
+  }
+}
+
+/**
+ * uint8_t pin   == pin number
+ * uint8_t count == how many times to blink
+ */
+void blinkSlowly(uint8_t pin, uint8_t count) {
+  for (int i = 0; i < count; i++) {
+    digitalWrite(pin, HIGH);
+    delay(350);
+    digitalWrite(pin, LOW);
+    delay(350);
+  }
+}
+
+void loop() {
+  blinkQuickly(redLED, 3);
+  blinkSlowly(redLED, 3);
+  blinkQuickly(redLED, 3);
+  delay(1000);
+}
